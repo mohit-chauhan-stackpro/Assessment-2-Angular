2567d7291d4a4339279eefb9058f225cc50dd49a
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-18 15:15:03 +0530

    added logic foe click button reset input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assessment-2-Angular';
+  username="";
+  buttonStatus=true;
+
+  enableButton(){
+    if(this.username.length>0){
+
+    }
+  }
+  resetInput(){
+    this.username="";
+  }
 }
